957184beaa02acc8a8ec686684373130ffd3ccf6
ヒルミーサクティ <abandel09@gmail.com>
2023-08-18 05:40:58 +0700

    Update and rename admin_panel_finder.py to apf.py

--- apf.py
+++ apf.py
@@ -6,14 +6,14 @@ from urllib2 import Request, urlopen, URLError, HTTPError
 def Space(j):
 	i = 0
 	while i<=j:
-		print " ",
+		print (" ",)
 		i+=1
 
 
 def findAdmin():
 	f = open("link.txt","r");
 	link = raw_input("Enter Site Name \n(ex : example.com or www.example.com ): ")
-	print "\n\nAvilable links : \n"
+	print ("\n\nAvilable links : \n")
 	while True:
 		sub_link = f.readline()
 		if not sub_link:
@@ -27,14 +27,14 @@ def findAdmin():
 		except URLError as e:
 			continue
 		else:
-			print "OK => ",req_link
+			print ("OK => ",req_link)
 
 def Credit():
-	Space(9); print "#####################################"
-	Space(9); print "#   +++ Admin Panel Finder v1 +++   #"
-	Space(9); print "#     Script by Illûmïnåté Ðëmøñ    #"
-	Space(9); print "#    Bangladesh Black Hat Hackers   #"
-	Space(9); print "#####################################"
+	Space(9); print ("#####################################")
+	Space(9); print ("#   +++ Admin Panel Finder v1 +++   #")
+	Space(9); print ("#     Script by Illûmïnåté Ðëmøñ    #")
+	Space(9); print ("#              HiSkayXD             #")
+	Space(9); print ("#####################################")
 
 Credit()
 findAdmin()
